59252b66007b6c930358e7851bcb91df1409a875
Akhilstaar <iakhilmeena@gmail.com>
2025-03-21 00:10:31 +0530

    sane logging now

--- Testing Code/runtime.cpp
+++ Testing Code/runtime.cpp
@@ -1,6 +1,13 @@
+// runtime.cpp
 #include <fstream>
+#include <iostream>
+#include <mutex>
+#include <thread>
+#include <chrono>
+#include <iomanip>
 
-static std::ofstream logFile("program.log");
+static std::ofstream logFile("program.log", std::ios::app);
+static std::mutex logMutex;
 
 extern "C" void logEvent(int eventType, void *addr, int line)
 {
@@ -8,7 +15,29 @@ extern "C" void logEvent(int eventType, void *addr, int line)
         "READ", "WRITE", "ACQUIRE", "RELEASE",
         "FORK", "JOIN", "ATOMIC_BEGIN", "ATOMIC_END"};
 
-    logFile << "[" << eventNames[eventType] << "] "
-            << "Address: " << addr << ", "
-            << "Line: " << line << "\n";
+    // Get thread ID
+    std::thread::id threadId = std::this_thread::get_id();
+
+    // Ensure thread safety while writing to log file
+    std::lock_guard<std::mutex> guard(logMutex);
+
+    if (eventType == 4) // FORK (Thread Begin)
+    {
+        logFile << "Thread begin: " << threadId << "\n";
+    }
+    else if (eventType == 5) // JOIN (Thread End)
+    {
+        logFile << "Thread ended: " << threadId << "\n";
+    }
+    else if (eventType == 2) // ACQUIRE (Lock Acquire)
+    {
+        logFile << "Lock acquire: TID: " << threadId << ", Lock address: " << addr << "\n";
+    }
+    else
+    {
+        logFile << "TID: " << threadId << ", "
+                << "Event: " << eventNames[eventType] << " "
+                << "ADDR: " << addr << ", "
+                << "Line: " << line << "\n";
+    }
 }
