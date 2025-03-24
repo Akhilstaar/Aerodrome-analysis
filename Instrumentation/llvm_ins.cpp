bf29c7f5f60ac72c47c917aa523c4c1de6b4a15c
Akhilstaar <iakhilmeena@gmail.com>
2025-03-24 23:52:53 +0530

    Fixed memory footprint, added child TID in thread begin and end events

--- Instrumentation/llvm_ins.cpp
+++ Instrumentation/llvm_ins.cpp
@@ -20,49 +20,69 @@ namespace
     PreservedAnalyses run(Module &M, ModuleAnalysisManager &)
     {
       LLVMContext &Context = M.getContext();
-      // DataLayout* dataLayout = new DataLayout(&M); // for tracking size of accesses
+      DataLayout* dataLayout = new DataLayout(&M); // for tracking size of accesses
 
       // log function types for each event
+
+      // For getting PID
+      FunctionType *pthreadSelfType = FunctionType::get(Type::getInt64Ty(Context), false);
+      FunctionCallee pthreadSelfFunc = M.getOrInsertFunction("pthread_self", pthreadSelfType);
+
       FunctionType *logReadType = FunctionType::get(
           Type::getVoidTy(Context),
-          {PointerType::get(Type::getInt8Ty(Context), 0),
-           Type::getInt32Ty(Context), Type::getInt32Ty(Context)},
+          { Type::getInt64Ty(Context),
+            PointerType::get(Type::getInt8Ty(Context), 0),
+            Type::getInt32Ty(Context), Type::getInt32Ty(Context)},
           false);
       FunctionCallee logReadFunc = M.getOrInsertFunction("logReadEvent", logReadType);
 
       FunctionType *logWriteType = FunctionType::get(
           Type::getVoidTy(Context),
-          {PointerType::get(Type::getInt8Ty(Context), 0),
+          { Type::getInt64Ty(Context),
+            PointerType::get(Type::getInt8Ty(Context), 0),
            Type::getInt32Ty(Context), Type::getInt32Ty(Context)},
           false);
       FunctionCallee logWriteFunc = M.getOrInsertFunction("logWriteEvent", logWriteType);
 
       FunctionType *logAcquireType = FunctionType::get(
           Type::getVoidTy(Context),
-          {PointerType::get(Type::getInt8Ty(Context), 0), Type::getInt32Ty(Context)},
+          { Type::getInt64Ty(Context),
+            PointerType::get(Type::getInt8Ty(Context), 0), 
+            Type::getInt32Ty(Context)},
           false);
       FunctionCallee logAcquireFunc = M.getOrInsertFunction("logAcquireEvent", logAcquireType);
 
       FunctionType *logReleaseType = FunctionType::get(
           Type::getVoidTy(Context),
-          {PointerType::get(Type::getInt8Ty(Context), 0), Type::getInt32Ty(Context)},
+          { Type::getInt64Ty(Context),
+            PointerType::get(Type::getInt8Ty(Context), 0), 
+            Type::getInt32Ty(Context)},
           false);
       FunctionCallee logReleaseFunc = M.getOrInsertFunction("logReleaseEvent", logReleaseType);
 
       FunctionType *logForkType = FunctionType::get(
-          Type::getVoidTy(Context), {Type::getInt32Ty(Context)}, false);
+          Type::getVoidTy(Context), 
+          { Type::getInt64Ty(Context), 
+            Type::getInt64Ty(Context), 
+            Type::getInt32Ty(Context)}, 
+          false);
       FunctionCallee logForkFunc = M.getOrInsertFunction("logForkEvent", logForkType);
 
       FunctionType *logJoinType = FunctionType::get(
-          Type::getVoidTy(Context), {Type::getInt32Ty(Context)}, false);
+          Type::getVoidTy(Context), 
+          { Type::getInt64Ty(Context), 
+            Type::getInt64Ty(Context), 
+            Type::getInt32Ty(Context)},
+          false);
       FunctionCallee logJoinFunc = M.getOrInsertFunction("logJoinEvent", logJoinType);
 
       FunctionType *logAtomicBeginType = FunctionType::get(
-          Type::getVoidTy(Context), {Type::getInt32Ty(Context)}, false);
+          Type::getVoidTy(Context), {
+            Type::getInt64Ty(Context), Type::getInt32Ty(Context)}, false);
       FunctionCallee logAtomicBeginFunc = M.getOrInsertFunction("logAtomicBeginEvent", logAtomicBeginType);
 
       FunctionType *logAtomicEndType = FunctionType::get(
-          Type::getVoidTy(Context), {Type::getInt32Ty(Context)}, false);
+          Type::getVoidTy(Context), {Type::getInt64Ty(Context), Type::getInt32Ty(Context)}, false);
       FunctionCallee logAtomicEndFunc = M.getOrInsertFunction("logAtomicEndEvent", logAtomicEndType);
 
       for (Function &F : M)
@@ -98,6 +118,11 @@ namespace
           {
             IRBuilder<> builder(I);
 
+            // TODO: Optimize this thing further. (if possible)
+            // Get the current TID
+            Value *tid = builder.CreateCall(pthreadSelfFunc);
+            tid = builder.CreateIntCast(tid, Type::getInt64Ty(Context), false);
+            
             // Get the source line number from debug metadata (if available).
             // probably usefull in finding what caused dataRace.
             unsigned line = 0;
@@ -107,20 +132,18 @@ namespace
 
             if (auto *loadInst = dyn_cast<LoadInst>(I))
             {
-              // For non-atomic loads, log a read event.
+              // TODO: consider atomic RMW's as well.
+
+              // For non-atomic loads, log a read event
               if (!loadInst->isAtomic())
               {
-                // I'm not sure if this is correct, ie. if it returns the size of load/store access
+                // I'm not sure if this is implemented correctly, ie. if it returns the size of load/store access
                 Type *ty = loadInst->getType();
-                unsigned size = ty->getPrimitiveSizeInBits() / 8;
                 Value *ptr = loadInst->getPointerOperand();
+                uint64_t size = dataLayout->getTypeSizeInBits(ty) / 8;
 
-                // this was in stackoverflow, doesn't work rn 
-                // PointerType *ptrType = cast<PointerType>(ptr->getType());
-                // uint64_t size = dataLayout->getTypeStoreSize(ptrType->getPointerElementType());
-                
                 Value *castedPtr = builder.CreateBitCast(ptr, PointerType::get(Type::getInt8Ty(Context), 0));
-                builder.CreateCall(logReadFunc, {castedPtr, ConstantInt::get(Type::getInt32Ty(Context), size), lineConstant});
+                builder.CreateCall(logReadFunc, {tid, castedPtr, ConstantInt::get(Type::getInt32Ty(Context), size), lineConstant});
               }
             }
             else if (auto *storeInst = dyn_cast<StoreInst>(I))
@@ -128,17 +151,14 @@ namespace
               // For non-atomic stores, log a write event.
               if (!storeInst->isAtomic())
               {
-                // I'm not sure if this is correct, ie. if it returns the size of load/store access
-                Type *ty = storeInst->getValueOperand()->getType();
-                unsigned size = ty->getPrimitiveSizeInBits() / 8;
+                // I'm not sure if this is implemented correctly, ie. if it returns the size of load/store access
                 Value *ptr = storeInst->getPointerOperand();
+                Value *storeValue = storeInst->getValueOperand();
+                Type *storeType = storeValue->getType();
+                uint64_t size = dataLayout->getTypeSizeInBits(storeType) / 8;
 
-                // this was in stackoverflow, doesn't work rn 
-                // PointerType *ptrType = cast<PointerType>(ptr->getType());
-                // uint64_t size = dataLayout->getTypeStoreSize(ptrType->getPointerElementType());
-                
                 Value *castedPtr = builder.CreateBitCast(ptr, PointerType::get(Type::getInt8Ty(Context), 0));
-                builder.CreateCall(logWriteFunc, {castedPtr, ConstantInt::get(Type::getInt32Ty(Context), size), lineConstant});
+                builder.CreateCall(logWriteFunc, {tid, castedPtr, ConstantInt::get(Type::getInt32Ty(Context), size), lineConstant});
               }
             }
             else if (auto *CI = dyn_cast<CallBase>(I))
@@ -146,30 +166,40 @@ namespace
               if (Function *called = CI->getCalledFunction())
               {
                 StringRef funcName = called->getName();
-                // Currently, acquire event: 
+
+                // Currently, acquire event:
                 // For mutex lock only
                 if (funcName == "pthread_mutex_lock" && CI->arg_size() >= 1)
                 {
                   Value *mutexArg = CI->getArgOperand(0);
                   Value *castedPtr = builder.CreateBitCast(mutexArg, PointerType::get(Type::getInt8Ty(Context), 0));
-                  builder.CreateCall(logAcquireFunc, {castedPtr, lineConstant});
+                  builder.CreateCall(logAcquireFunc, {tid, castedPtr, lineConstant});
                 }
 
-                // Currently, release event: 
+                // Currently, release event:
                 // For mutex unlock only
                 else if (funcName == "pthread_mutex_unlock" && CI->arg_size() >= 1)
                 {
                   Value *mutexArg = CI->getArgOperand(0);
                   Value *castedPtr = builder.CreateBitCast(mutexArg, PointerType::get(Type::getInt8Ty(Context), 0));
-                  builder.CreateCall(logReleaseFunc, {castedPtr, lineConstant});
+                  builder.CreateCall(logReleaseFunc, {tid, castedPtr, lineConstant});
                 }
                 else if (funcName == "pthread_create")
                 {
-                  builder.CreateCall(logForkFunc, {lineConstant});
+                  Value *parentTID = tid; // Current thread is the parent ( ? )
+                  Value *threadArg = CI->getArgOperand(0);
+
+                  // Load child TID after pthread_create
+                  builder.SetInsertPoint(I->getNextNode());
+                  Value *childTID = builder.CreateLoad(Type::getInt64Ty(Context), threadArg);
+                  builder.CreateCall(logForkFunc, {parentTID, childTID, lineConstant});
                 }
-                else if (funcName == "pthread_join")
+                else if (funcName == "pthread_join" && CI->arg_size() >= 1)
                 {
-                  builder.CreateCall(logJoinFunc, {lineConstant});
+                  Value *parentTID = tid; // Current thread is the parent ( ? )
+                  Value *childTID = CI->getArgOperand(0);
+                  childTID = builder.CreateIntCast(childTID, Type::getInt64Ty(Context), false);
+                  builder.CreateCall(logJoinFunc, {parentTID, childTID, lineConstant});
                 }
                 else if (funcName == "atomic_begin")
                 {
--- Testing Code/runtime.cpp
+++ Testing Code/runtime.cpp
@@ -1,50 +1,47 @@
-// runtime.cpp
 #include <fstream>
 #include <iostream>
 #include <mutex>
-#include <thread>
-#include <iomanip>
+#include <pthread.h>
 
 static std::ofstream logFile("program.log", std::ios::app);
 static std::mutex logMutex;
 
-extern "C" void logReadEvent(void *addr, int size, int line) {
+extern "C" void logReadEvent(long long tid, void *addr, int size, int line) {
     std::lock_guard<std::mutex> guard(logMutex);
-    logFile << "TID: " << std::this_thread::get_id() << ", Event: READ ADDR: " << addr << ", Size (B): " << size << ", Line: " << line << "\n";
+    logFile << "TID: " << tid << ", Event: READ ADDR: " << addr << ", Size (B): " << size << ", Line: " << line << "\n";
 }
 
-extern "C" void logWriteEvent(void *addr, int size, int line) {
+extern "C" void logWriteEvent(long long tid, void *addr, int size, int line) {
     std::lock_guard<std::mutex> guard(logMutex);
-    logFile << "TID: " << std::this_thread::get_id() << ", Event: WRITE ADDR: " << addr << ", Size (B): " << size << ", Line: " << line << "\n";
+    logFile << "TID: " << tid << ", Event: WRITE ADDR: " << addr << ", Size (B): " << size << ", Line: " << line << "\n";
 }
 
-extern "C" void logAcquireEvent(void *addr, int line) {
+extern "C" void logAcquireEvent(long long tid, void *addr, int line) {
     std::lock_guard<std::mutex> guard(logMutex);
-    logFile << "Lock acquire: TID: " << std::this_thread::get_id() << ", Lock address: " << addr << " at line " << line << "\n";
+    logFile << "Lock acquire: TID: " << tid << ", Lock address: " << addr << " at line " << line << "\n";
 }
 
-extern "C" void logReleaseEvent(void *addr, int line) {
+extern "C" void logReleaseEvent(long long tid, void *addr, int line) {
     std::lock_guard<std::mutex> guard(logMutex);
-    logFile << "Lock release: TID: " << std::this_thread::get_id() << ", Lock address: " << addr << " at line " << line << "\n";
+    logFile << "Lock release: TID: " << tid << ", Lock address: " << addr << " at line " << line << "\n";
 }
 
-// TODO: Currently Returns parent id, fix it to return both child TID & parent TID
-extern "C" void logForkEvent(int line) {
+extern "C" void logForkEvent(long long parentTID, long long childTID, int line) {
     std::lock_guard<std::mutex> guard(logMutex);
-    logFile << "Thread begin: " << std::this_thread::get_id() << " at line " << line << "\n";
+    logFile << "Thread begin: Parent TID: " << parentTID << ", Child TID: " << childTID << " at line " << line << "\n";
 }
 
-extern "C" void logJoinEvent(int line) {
+extern "C" void logJoinEvent(long long parentTID, long long childTID, int line) {
     std::lock_guard<std::mutex> guard(logMutex);
-    logFile << "Thread ended: " << std::this_thread::get_id() << " at line " << line << "\n";
+    logFile << "Thread ended: Parent TID: " << parentTID << ", Child TID: " << childTID << " at line " << line << "\n";
 }
 
-extern "C" void logAtomicBeginEvent(int line) {
+extern "C" void logAtomicBeginEvent(long long tid, int line) {
     std::lock_guard<std::mutex> guard(logMutex);
-    logFile << "TID: " << std::this_thread::get_id() << ", Event: ATOMIC_BEGIN Line: " << line << "\n";
+    logFile << "TID: " << tid << ", Event: ATOMIC_BEGIN Line: " << line << "\n";
 }
 
-extern "C" void logAtomicEndEvent(int line) {
+extern "C" void logAtomicEndEvent(long long tid, int line) {
     std::lock_guard<std::mutex> guard(logMutex);
-    logFile << "TID: " << std::this_thread::get_id() << ", Event: ATOMIC_END Line: " << line << "\n";
+    logFile << "TID: " << tid << ", Event: ATOMIC_END Line: " << line << "\n";
 }
